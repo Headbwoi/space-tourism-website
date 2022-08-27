88c26c48e03ab55e1737279a6b208853d939e4a8
headbwoi <tochukwujohn24@gmail.com>
2022-08-27 12:37:10 +0100

    moved home and destination assets folders to public

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/NavBar"
 
 const Destination = () => {
   return (
-    <main>
+    <main className="pt-6 lg:pt-10 h-[53.125rem] md:h-[64rem] lg:h-[56.25rem]">
       <Navbar />
     </main>
   )
