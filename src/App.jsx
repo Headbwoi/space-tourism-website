64d17190eb6c1bc43e38a98077850ddc9e1515f1
headbwoi <tochukwujohn24@gmail.com>
2022-08-28 10:09:37 +0100

    finally fetched/imported  data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Home, Destination, Crew, Technology, NotFound } from "./pages"
+import data from "./data.json"
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
+        <Route
+          path="/destination"
+          element={<Destination destination={data.destinations} />}
+        />
         <Route path="/crew" element={<Crew />} />
         <Route path="/technology" element={<Technology />} />
         <Route path="*" element={<NotFound />} />
--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,10 +1,33 @@
 import Navbar from "../../components/NavBar"
+import { useState } from "react"
 
-const Destination = () => {
+const Destination = ({ destination }) => {
+  const [index, setIndex] = useState(0)
+  let data = destination[index]
+
+  window.onload = () => setIndex(0)
+
+  console.log(data.name)
+
   return (
-    <main className="pt-6 lg:pt-10 h-[53.125rem] md:h-[64rem] lg:h-[56.25rem]">
+    <div className="pt-6 lg:pt-10 h-[53.125rem] md:h-[64rem] lg:h-[56.25rem] bg-[url(/assets/destination/background-destination-mobile.jpg)] md:bg-[url(/assets/destination/background-destination-tablet.jpg)] lg:bg-[url(/assets/destination/background-destination-desktop.jpg)] bg-no-repeat bg-cover ">
       <Navbar />
-    </main>
+      <div className="bg-white text-darkBlue h-20 w-full">
+        <div className="flex items-center justify-between">
+          <span onClick={() => setIndex(0)}>0</span>
+          <span onClick={() => setIndex(1)}>1</span>
+          <span onClick={() => setIndex(2)}>2</span>
+          <span onClick={() => setIndex(3)}>3</span>
+        </div>
+
+        <div className="text-white">
+          <h1>{data.name}</h1>
+          <img src={data.images.webp} alt="" />
+          <p>{data.description}</p>
+          <p>{data.travel}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
