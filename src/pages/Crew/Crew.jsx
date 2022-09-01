2da37259b4d68a8d21529bfe04a27f93e2548411
headbwoi <tochukwujohn24@gmail.com>
2022-09-01 04:49:40 +0100

    edited navbar line

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -1,10 +1,14 @@
 import "./crew.css"
 import Navbar from "../../components/NavBar"
+import Header from "../../components/Header"
 
 const Crew = () => {
   return (
     <section className="crew-home">
       <Navbar />
+
+      <Header num="02" text="meet your crew" />
+      <div className="crew-container"></div>
     </section>
   )
 }
